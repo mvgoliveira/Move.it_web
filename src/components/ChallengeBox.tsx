8066a9cb4637c157fb8c7a6b619e8b7b3791399e
MarcusOliveira <Markusvi17@gmail.com>
2021-02-24 13:18:37 -0300

    Aula 3 - Context api

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,47 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import  styles from '../styles/components/ChallengeBox.module.css';
+
+export function ChallengeBox() {
+   const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+
+   return (
+      <div className={styles.ChallengeBoxContainer}>
+         { activeChallenge ? (
+            <div className={styles.ChallengeActive}>
+               <header> Ganhe { activeChallenge.amount } xp</header>
+
+               <main>
+                  <img src={`icons/${activeChallenge.type}.svg`} alt=""/>
+                  <strong> { activeChallenge.type } </strong>
+                  <p> { activeChallenge.description } </p>
+               </main>
+
+               <footer>
+
+                  <button 
+                     type="button" 
+                     className={styles.ChallengeFailedButton} 
+                     onClick={resetChallenge}
+                  >
+                     Falhei
+                  </button>
+
+                  <button type="button" className={styles.ChallengeSucceededButton}>Completei</button>
+
+               </footer>
+            </div>
+         ) : (
+            <div className={styles.ChallengeNotActive}>
+               <strong>
+                  Finalize um ciclo para receber um desafio
+               </strong>
+               <p>
+                  <img src="icons/level-up.svg" alt="Level Up"/>
+                  Avançe de level completando desafios.
+               </p>
+            </div>
+         )}
+      </div>
+   );
+} 
