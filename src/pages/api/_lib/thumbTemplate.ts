ad620124b695ee66516fe9808b0978ac0095e21f
MarcusOliveira <Markusvi17@gmail.com>
2021-02-28 19:49:23 -0300

    Add Thumbnail serverless

---
+++ src/pages/api/_lib/thumbTemplate.ts
@@ -0,0 +1,134 @@
+export default function getThumbnailTemplate(level: string, challenges: string, exp: string) {
+   return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      
+      <title>Thumbnail</title>
+      
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+
+      <style>
+         * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+         }
+
+         body {
+            display: flex;
+            font: 400 1rem "Inter", sans-serif;
+            color: #2E384D;
+            background: #fff;
+            height: 100vh;
+
+            margin: 0 50px;
+            
+            line-height: 1;
+         }
+
+         #leftContainer {
+            width: 100%;
+
+            display: flex;
+            flex-direction: column;
+            
+            justify-content: center;
+            align-items: center;
+
+            text-align: center;
+         }
+
+         #leftContainer header{
+            color: #5965E0;
+            font-size: 19.129rem;
+            font-weight: 600;
+         }
+
+         #leftContainer p {
+            max-width: 408px;
+            font-size: 3.5rem;
+            font-weight: bold;
+         }
+
+         #rightContainer {
+            width: 100%;
+
+            display: flex;
+            flex-direction: column;
+            
+            justify-content: center;
+            align-items: center;
+
+            text-align: start;
+         }
+
+         #rightContainer > div {
+            width: 342px;
+         }
+         
+         #rightContainer span {
+            display: block;
+            color: rgb(102, 102, 102, 0.5);
+            font-size: 1.5rem;
+            font-weight: 600;
+            margin-bottom: 15px;
+         }
+
+         #rightContainer p {
+            border-top: 50px;
+            color: #666;
+            font-size: 2.5rem;
+            font-weight: 500;
+         }
+
+         #rightContainer strong {
+            color: #5965E0;
+            font-weight: 500;
+         }
+
+         #desafios {
+            padding-bottom: 32px;
+            border-bottom: 2px solid #DCDDE0;
+         }
+
+         #experiencia {
+            margin-top: 32px;
+            padding-bottom: 32px;
+            border-bottom: 2px solid #DCDDE0;
+         }
+
+         #logo {
+            margin-top: 84px;
+         }
+
+      </style>
+      </head>
+      <body>
+         <div id="leftContainer">
+            <header>${level}</header>
+            <p>Avancei para o próximo level</p>
+         </div>
+
+         <div id="rightContainer">
+            <div>
+               <div id="desafios">
+                  <span>DESAFIOS</span>
+                  <p><strong>${challenges}</strong> completados</p>
+               </div>
+   
+               <div id="experiencia">
+                  <span>EXPERIÊNCIA</span>
+                  <p><strong>${exp}</strong> xp</p>
+               </div>
+   
+               <div id="logo">Logo</div>
+            </div>
+         </div>
+      </body>
+      </html>
+
+   `;
+}
---
+++ src/pages/api/thumbnail.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getScreenshot } from "./_lib/chromiun";
+import getThumbnailTemplate from "./_lib/thumbTemplate";
+
+const isDev = !process.env.AWS_REGION;
+
+export default async function (req : NextApiRequest, res: NextApiResponse) {
+   try {
+      const level = String(req.query.level)
+      const challenges = String(req.query.challenges)
+      const exp = String(req.query.exp)
+
+      if (!level) {
+         throw new Error("Title is require");
+      }
+
+      const html = getThumbnailTemplate(level, challenges, exp);
+
+      const file = await getScreenshot(html, isDev);
+
+      res.setHeader('Content-Type', 'image/png');
+      res.setHeader('Cache-Control', 'public, immutable, no-transform, s-maxage=31536000, max-age=31536000');
+      
+      return res.end(file);
+
+   } catch (error) {
+      console.error(error);
+      
+      res.status(500).send('Internal server error');
+   }
+}
