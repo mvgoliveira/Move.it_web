f9f66e4bb5eacba5b62aec4128352da4ef1f4e61
MarcusOliveira <MARKUSVI17@GMAIL.COM>
2021-03-05 18:48:12 -0300

    Added thumbnail function to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,10 +31,32 @@ export default function Index(props : HomeProps) {
     return <Redirect to="/home" />
   }
 
+  const baseUrl = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : `https://${process.env.VERCEL_URL}`;
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Move.it</title>
+        <meta name="description" content="{props.description}" />
+
+        <meta property="og:site_name" content="Move.it" />
+
+        <meta property="og:title" content="Avancei para o próximo nível" />
+        <meta property="og:description" content="Junte-se a mim no move.it, um site com o objetivo de ajudar pessoas que precisam usar computadores por tempos prolongados." />
+
+        <meta property="og:image" content={`${baseUrl}/api/thumbnail.png?level=${props.level}&challenges=${props.challengesCompleted}&exp=${props.currentExperience}`} />
+        <meta property="og:image:type" content="image/png" />
+
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Avancei para o próximo nível" />
+        <meta name="twitter:description" content="Junte-se a mim no move.it, um site com o objetivo de ajudar pessoas que precisam usar computadores por tempos prolongados." />
+        
+        <meta name="twitter:image" content={`${baseUrl}/api/thumbnail.png?level=${props.level}&challenges=${props.challengesCompleted}&exp=${props.currentExperience}`} />
       </Head>
       <IndexBox />
     </div>
