b6f135507aab8f3fe62abd955637f717a3428581
MarcusOliveira <MARKUSVI17@GMAIL.COM>
2021-03-18 20:17:06 -0300

    fix base url

--- src/components/LoginBox.tsx
+++ src/components/LoginBox.tsx
@@ -2,6 +2,9 @@ import styles from '../styles/pages/Login.module.css';
 import { signIn } from 'next-auth/client';
 
 export function LoginBox() {
+   const baseUrl = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : `https://${process.env.VERCEL_URL}`;
 
    return (
       <div className={styles.container}>
@@ -19,7 +22,7 @@ export function LoginBox() {
                   className={styles.buttonActive} 
                   onClick={(): Promise<void> => signIn(
                      'github', 
-                     { callbackUrl: 'http://localhost:3000/home'}
+                     { callbackUrl: `${baseUrl}/home`}
                   )}>
 
                   <img width={30} src="/icons/github-logo.svg" alt="github Logo"/>
