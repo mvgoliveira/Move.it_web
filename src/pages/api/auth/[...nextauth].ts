6a00e01faf8925ea7f83a8fafeed3f8ff5773eac
MarcusOliveira <MARKUSVI17@GMAIL.COM>
2021-03-18 21:53:21 -0300

    changed clientsecret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,7 +7,7 @@ const options = {
   providers: [
     Providers.GitHub({
       clientId: "71739218ccf03e39cc9b",
-      clientSecret: "b3309fcaf8660d3f95bd10c8e57729d9b9a55504",
+      clientSecret: "d8423b76ebd5df3d568ef2380aa87cdbff92dc72",
     }),
   ]
 };
