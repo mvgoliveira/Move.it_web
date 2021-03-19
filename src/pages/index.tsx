35694bcf63f430ab4658bd9b158949fe87f1f568
MarcusOliveira <MARKUSVI17@GMAIL.COM>
2021-03-18 21:13:58 -0300

    update dinamic url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ export default function Index(props : HomeProps) {
 
   const baseUrl = process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : `https://${process.env.VERCEL_URL}`;
+    : `https://${process.env.SITE_URL}`;
 
   return (
     <div className={styles.container}>
