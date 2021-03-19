539bccaa1fd642d10c9a96f001fbfe20c9d58fb0
MarcusOliveira <MARKUSVI17@GMAIL.COM>
2021-03-18 23:53:41 -0300

    Fix bugs in github oauth - netlify deploy

--- src/components/LoginBox.tsx
+++ src/components/LoginBox.tsx
@@ -2,9 +2,6 @@ import styles from '../styles/pages/Login.module.css';
 import { signIn } from 'next-auth/client';
 
 export function LoginBox() {
-   const baseUrl = process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : `https://${process.env.VERCEL_URL}`;
 
    return (
       <div className={styles.container}>
@@ -22,7 +19,7 @@ export function LoginBox() {
                   className={styles.buttonActive} 
                   onClick={(): Promise<void> => signIn(
                      'github', 
-                     { callbackUrl: `${baseUrl}/home`}
+                     { callbackUrl: 'https://moveitapp.netlify.app/home'}
                   )}>
 
                   <img width={30} src="/icons/github-logo.svg" alt="github Logo"/>
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,8 +6,8 @@ import Providers from 'next-auth/providers';
 const options = {
   providers: [
     Providers.GitHub({
-      clientId: "71739218ccf03e39cc9b",
-      clientSecret: "d8423b76ebd5df3d568ef2380aa87cdbff92dc72",
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ]
 };
